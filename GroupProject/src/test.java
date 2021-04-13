da502eef6cd8e409f275dcfcd13dadf69b9f71a5
aaronByrne1 <65355719+aaronByrne1@users.noreply.github.com>
2021-04-13 20:42:52 +0200

    Added TST From the code in the book

--- GroupProject/src/test.java
+++ GroupProject/src/test.java
@@ -1,9 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		System.out.println("Hello people");
+		TST<Integer> searchTree = new TST<Integer>();
+		
+		File file =new File("stops.txt");
+		Scanner s= new Scanner(file);
+		s.nextLine();
+		while(s.hasNextLine()) {
+			String key;
+			int value;
+			String arr[]= s.nextLine().split(",");
+			key=arr[2];
+			value=Integer.parseInt(arr[1]);
+			System.out.println(value);
+		}
 	}
 
 }
