b6f2743fef9449ede2f7f9469fdc5f8e4619cc99
Stephen <cashins@tcd.ie>
2021-04-29 16:59:58 +0100

    added parsing for head_sign

--- stopTime.java
+++ stopTime.java
@@ -6,11 +6,12 @@ public class stopTime {
 	private LocalTime departure_time;
 	private int stop_id;
 	private int stop_sequence;
+	private int stop_headsign;
 	private int pickup_type;
 	private int drop_off_type;
 	private float shape_dist_traveled;
-	
-	stopTime(int trip_id, LocalTime arrival_time, LocalTime departure_time, int stop_id, int stop_sequence,
+
+	stopTime(int trip_id, LocalTime arrival_time, LocalTime departure_time, int stop_id, int stop_sequence, String stop_headsign,
 			 int pickup_type, int drop_off_type, float shape_dist_traveled) {
 			
 		this.trip_id = trip_id;
@@ -18,7 +19,11 @@ public class stopTime {
 		this.departure_time = departure_time;
 		this.stop_id = stop_id;
 		this.stop_sequence = stop_sequence;
-		//this.stop_headsign = stop_headsign;
+		try {
+			this.stop_headsign = Integer.parseInt(stop_headsign);
+		} catch(NumberFormatException e) {
+			this.stop_headsign = 0;
+		}
 		this.pickup_type = pickup_type;
 		this.drop_off_type = drop_off_type;
 		this.shape_dist_traveled = shape_dist_traveled;		
@@ -27,7 +32,7 @@ public class stopTime {
 	public void print()
 	{
 		System.out.print(trip_id + " " + arrival_time + " " + " " + departure_time + " " + stop_id + " " + stop_sequence + " " + pickup_type + " "
-				+ drop_off_type + " " + shape_dist_traveled + "\n");
+				+ stop_headsign + " " + drop_off_type + " " + shape_dist_traveled + "\n");
 	}
 	
 	public LocalTime getArrivalTime()
